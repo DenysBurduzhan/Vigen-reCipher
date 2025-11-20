4de0363f6769e5e90fd7a6be7124a8cf4f1b6505
Denys <denisburduzan@gmail.com>
2025-11-20 17:13:27 +0200

    added decrypt(will be simplified) method and extract key to separate method(keyToWordLength)

--- src/main/java/Cipher/Cipher.java
+++ src/main/java/Cipher/Cipher.java
@@ -11,12 +11,12 @@ public class Cipher {
         String key = scan.nextLine();
         String word = scan.nextLine();
 
-            System.out.println(encrypt(key, word));
+        System.out.println(encrypt(key, word));
+        String encryptedWord = encrypt(key, word);
+        System.out.println(decrypt(key, word, encryptedWord));
     }
 
-    public static String encrypt(String key, String word) {
-        // lemon helloworld
-
+    public static char[] keyToWordLength(String key, String word){
         char[] str = null;
         if (word.length() > key.length()) {
             str = new char[word.length()];
@@ -24,14 +24,39 @@ public class Cipher {
                 str[i] = key.charAt(i);
             }
             for(int f = 0; f < word.length() - key.length(); f++ ){
-                    str[key.length() + f] = key.charAt(f);
+                str[key.length() + f] = key.charAt(f);
+            }
+        }
+        return str;
+    }
+    public static String decrypt(String key, String word, String encryptedWord){
+        char[] newKey = keyToWordLength(key, word);
+        StringBuilder builder = new StringBuilder();
+        int indexSet = 0;
+        for (int i = 0; i < word.length(); i++) {
+            int index1 = findIndex(Constants.getEng(), newKey[i]);
+            int index2 = findIndex(Constants.getEng(), encryptedWord.charAt(i));
+            indexSet += index2 - index1;
+            if(indexSet < 0) {
+                indexSet = indexSet + 26;
+                builder.append(Constants.getEng().get(indexSet));
+                indexSet = 0;
+            }else{
+                builder.append(Constants.getEng().get(indexSet));
+                indexSet = 0;
             }
         }
 
+        return builder.toString();
+    }
+
+    public static String encrypt(String key, String word) {
+        // lemon helloworld
+        char[] newKey = keyToWordLength(key, word);
         StringBuilder builder = new StringBuilder();
         int indexSum = 0;
         for (int i = 0; i < word.length(); i++) {
-            int index1 = findIndex(Constants.getEng(), str[i]);
+            int index1 = findIndex(Constants.getEng(), newKey[i]);
             int index2 = findIndex(Constants.getEng(), word.charAt(i));
             indexSum += index1 + index2;
             if(indexSum > 25) {
