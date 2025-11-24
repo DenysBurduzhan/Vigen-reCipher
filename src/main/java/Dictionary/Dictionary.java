e4d3dbfabf2ed879e1ba706a055801b39fa750c5
Denys <denisburduzan@gmail.com>
2025-11-24 18:43:25 +0200

    @Setter fo Dictionary

--- src/main/java/Dictionary/Dictionary.java
+++ src/main/java/Dictionary/Dictionary.java
@@ -1,22 +1,16 @@
 package Dictionary;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Arrays;
 
 public class Dictionary {
     @Getter
+    @Setter
     private static  String[] dictionary = new String[]{"take", "or", "for", "lemon" , "hello", "of", "as", "his", "that", "he", "was", "for", "on", "are", "with", "they", "be", "at", "by", "word", "have", "play", "my", "world", "there", "those", "victory", "is", "not", "to", "in", "the"};
 
     @Getter
+    @Setter
     private static  String[] dictionaryUKR = new String[]{"Ми", "всі", "щось", "Привіт", "хто", "що", "коли", "слово", "ти", "він", "вона", "воно", "вони", "йому", "робить", "працює", "каже", "розмовляє", "співає", "чує", "що робить", "збирає", "поле", "був", "була", "ні", "так", "тому що"};
-
-
-    public static void setDictionary(String[] dictionary) {
-        Dictionary.dictionary = dictionary;
-    }
-
-    public static void setDictionaryUKR(String[] dictionaryUKR) {
-        Dictionary.dictionaryUKR = dictionaryUKR;
-    }
 }
