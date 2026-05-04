d6496606576d89755ce76b3b594bb69d4813faa6
Denys <denisburduzan@gmail.com>
2026-05-04 20:57:42 +0300

    Added BruteForceImplTest

--- src/test/java/ImplementationsTest/BruteForceImplTest.java
+++ src/test/java/ImplementationsTest/BruteForceImplTest.java
@@ -1,17 +1,19 @@
 package ImplementationsTest;
+import static org.junit.jupiter.api.Assertions.*;
 
+import BruteForce.BruteForceImpl;
+import Dictionary.Dictionary;
 import org.junit.jupiter.api.Test;
 
 public class BruteForceImplTest {
+    BruteForceImpl bruteForceImpl = new BruteForceImpl();
     @Test
-    public void testDictionaryByLang(){
+    public void testGetLanguage(){
+        assertEquals(Dictionary.getDictionary(),bruteForceImpl.getLanguage("English"));
     }
 
     @Test
-    public void testKeyFinder(){
-    }
-
-    @Test
-    public void testMatchesCounter(){
+    public void testFindKey(){
+        assertEquals("good", bruteForceImpl.findKey("key: good text: I love to hear her speak, yet well I know"));
     }
 }
