403c385b2105fd4d7c5ee6b1060bf514fbbe7dcf
Denys Burduzhan <denisburduzan@gmail.com>
2026-05-06 18:21:30 +0300

    added SettingsManagerImplTest (tested index, register, get language)

--- src/test/java/ImplementationsTest/SettingsManagerImplTest.java
+++ src/test/java/ImplementationsTest/SettingsManagerImplTest.java
@@ -1,32 +1,33 @@
 package ImplementationsTest;
+import static org.junit.jupiter.api.Assertions.*;
 
+import Constants.Constants;
 import CyphersOptions.SettingsManagerImpl;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class SettingsManagerImplTest {
+    SettingsManagerImpl settingsManagerImpl = new SettingsManagerImpl();
     @Test
     public void testRegisterCheck() {
-        SettingsManagerImpl settingsManagerImpl = new SettingsManagerImpl();
+        assertEquals(List.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'),
+                settingsManagerImpl.registerCheck(settingsManagerImpl.getLanguage("ABCD"), 'D'));
     }
 
     @Test
     public void testFindIndex() {
-        SettingsManagerImpl settingsManagerImpl = new SettingsManagerImpl();
+        assertEquals(1, settingsManagerImpl.findIndex(Constants.getEng(), 'B'));
     }
 
     @Test
     public void testKeyToWordLength() {
-        SettingsManagerImpl settingsManagerImpl = new SettingsManagerImpl();
+        char[] c = settingsManagerImpl.keyToWordLength("a", "abc");
+        assertEquals(3, c.length);
     }
 
     @Test
     public void testGetLanguage() {
-        SettingsManagerImpl settingsManagerImpl = new SettingsManagerImpl();
+        assertEquals(Constants.getEng(), settingsManagerImpl.getLanguage("eng"));
     }
-
-    @Test
-    public void testProcess() {
-        SettingsManagerImpl settingsManagerImpl = new SettingsManagerImpl();
-    }
-
 }
