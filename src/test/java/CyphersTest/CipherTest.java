e2e5ff3e6d6154f7a68da2b1d8275fc51cc067bf
Denys Burduzhan <denisburduzan@gmail.com>
2026-05-07 16:47:40 +0300

    simplified CipherTest

--- src/test/java/CyphersTest/CipherTest.java
+++ src/test/java/CyphersTest/CipherTest.java
@@ -9,18 +9,21 @@ import org.junit.jupiter.api.Test;
 
 
 public class CipherTest {
+    String text = "I love to hear her speak, yet well I know";
+    String key = "good";
+    String cipher = "O zcyk hc kkof kkf gskoy, esh zkzz L qbcz";
     @Test
     public void testBruteforce() {
-        assertEquals("key: good text: I love to hear her speak, yet well I know", BruteForce.bruteforce("O zcyk hc kkof kkf gskoy, esh zkzz L qbcz"));
+        assertEquals("key: " + key + " text: " + text, BruteForce.bruteforce(cipher));
     }
 
     @Test
     public void testDecrypt() {
-        assertEquals("I love to hear her speak, yet well I know", Decryptor.decrypt("good", "O zcyk hc kkof kkf gskoy, esh zkzz L qbcz"));
+        assertEquals(text, Decryptor.decrypt(key, cipher));
     }
 
     @Test
     public void testEncrypt() {
-        assertEquals("O zcyk hc kkof kkf gskoy, esh zkzz L qbcz", Encryptor.encrypt("good", "I love to hear her speak, yet well I know"));
+        assertEquals(cipher, Encryptor.encrypt(key, text));
     }
 }
